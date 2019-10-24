17ec0f4c6112fe798c5a336dbeae3f9c46f0cf7b
Maria <maria.karamina@gmail.com>
2019-10-24 15:23:07 +0300

    Name changes in radix_sort

--- radix_sort.c
+++ radix_sort.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "GeneralHeader.h"
 
-void copy_array(relation *initial, relation *result, uint64_t start_index, uint64_t end_index)
+void copy_relation(relation *source, relation *result, uint64_t start_index, uint64_t end_index)
 {
 	for(int i=start_index; i<end_index; i++)
 	{
@@ -51,31 +51,34 @@ int copy_relation_with_psum(relation* source, relation* target,uint64_t index_st
     return 0;
 }
 
-void radix_sort(unsigned short byte, relation *initial, relation *result, uint64_t start_index, uint64_t end_index)
+void radix_sort(unsigned short byte, relation *source, relation *result, uint64_t start_index, uint64_t end_index)
 {
-	if(end_index - start_index < 64KB)  //TODO
+	//Check if bucket is small enough
+	if((end_index - start_index)*sizeof(tuple) < 64*1024)
 	{
+		//Choose whether to place result in source or result array
 		if(byte % 2 == 0)
 		{
-			quicksort(initial, start_index, end_index);
-			copy_array(initial, result, start_index, end_index);
+			quicksort(source, start_index, end_index);
+			copy_relation(source, result, start_index, end_index);
 		}
 		else
 		{
-			quicksort(initial, end_index);
+			quicksort(source, end_index);
 		}
 	}
 	else
 	{
 		histogram *hist = malloc(sizeof(histogram));
-		create_histogram_from_relation(initial, start_index, end_index, byte);
+		create_histogram_from_relation(source, start_index, end_index, byte);
 		transform_histogram_to_cumulative_histogram(hist);
-		split_and_fill_array(initial, result, start_index, end_index, hist);
+		copy_relation_with_psum(source, result, start_index, end_index, hist, byte);
 		
+		//Recursively sort every part of the array
 		uint64_t start = start_index;
 		for(int i=0; i<HIST_SIZE; i++)
 		{
-			radix_sort(byte+1, result, initial, start, hist->array[i]);
+			radix_sort(byte+1, result, source, start, hist->array[i]);
 			start = hist->array[i];
 		}
 	}

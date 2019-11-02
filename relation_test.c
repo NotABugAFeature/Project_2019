4b7ec75ce8bd8c9d3ec8b383942ae03ae4532c07
Maria <maria.karamina@gmail.com>
2019-11-02 15:45:05 +0200

    relation_test.c half done

--- relation_test.c
+++ relation_test.c
@@ -17,15 +17,83 @@ int clean_suite(void)
     return 0;
 }
 
-void testRead_from_file()
+
+/**
+ * Test case 1: File does not exist
+ */
+void testRead_from_file1()
 {
-    char* p2;
-    table* result=read_from_file(p0);
-    if(1 /*check result*/)
+    char *filename = "/test_files/doesnt_exist.txt";
+    table* t=read_from_file(filename);
+    if(t != NULL)
     {
         CU_ASSERT(0);
     }
 }
+
+/**
+ * Test case 2: Empty file
+ */
+void testRead_from_file2()
+{
+    char *filename = "/test_files/empty_file.txt";
+    table* t=read_from_file(filename);
+    if(t != NULL)
+    {
+        CU_ASSERT(0);
+    }
+}
+
+/**
+ * Test case 3: Wrong column-row numbers
+ */
+void testRead_from_file3()
+{
+    char *filename = "/test_files/fail_table_test_file.txt";
+    table* t=read_from_file(filename);
+    if(t != NULL)
+    {
+        CU_ASSERT(0);
+    }
+}
+
+/**
+ * Test case 4: 0 rows
+ */
+void testRead_from_file4()
+{
+    char *filename = "/test_files/empty_table_test_file.txt";
+    table* t=read_from_file(filename);
+    if(t == NULL || t->rows != 0 || t->table != NULL)
+    {
+        CU_ASSERT(0);
+    }
+}
+
+/**
+ * Test case 5: Normal file with 4 rows, 3 columns
+ */
+void testRead_from_file4()
+{
+    char *filename = "/test_files/small_table_test_file.txt";
+    table* t=read_from_file(filename);
+    if(t == NULL || t->rows != 3 || t->columns != 4 || t->table == NULL)
+    {
+        CU_ASSERT(0);
+    }
+
+    for(uint64_t i=0; i<3; i++)
+    {
+    	for(uint64_t j=0; j<4; j++)
+    	{
+    		if(t->table[i][j] != i+j)
+    		{
+    			CU_ASSERT(0);
+    		}
+    	}
+    }
+}
+
 
 void testDelete_table()
 {
@@ -48,6 +116,90 @@ void testCreate_relation_from_table()
         CU_ASSERT(0);
     }
 }
+
+/**
+ * Test case 1: File does not exist
+ */
+void testRelation_from_file1()
+{
+	char *filename = "/test_files/doesnt_exist.txt";
+	relation *rel=relation_from_file(filename);
+	if(relation != NULL)
+	{
+		CU_ASSERT(0);
+	}
+}
+
+/**
+ * Test case 2: File has only one column
+ */
+void testRelation_from_file2()
+{
+	char *filename = "/test_files/fail_relation_from_file.txt";
+	relation *rel=relation_from_file(filename);
+	if(relation != NULL)
+	{
+		CU_ASSERT(0);
+	}
+}
+
+/**
+ * Test case 3: Empty file
+ */
+void testRelation_from_file3()
+{
+	char *filename = "/test_files/empty_file.txt";
+	relation *rel=relation_from_file(filename);
+	if(relation == NULL || relation->num_tuples != 0 || relation->tuples != NULL)
+	{
+	    CU_ASSERT(0);
+	}
+}
+
+/**
+ * Test case 4: Normal file with 5 lines
+ */
+void testRelation_from_file4()
+{
+	char *filename = "/test_files/small_relation_test_file.txt";
+	relation *rel=relation_from_file(filename);
+	if(relation == NULL || relation->num_tuples != 5 || relation->tuples == NULL)
+	{
+	    CU_ASSERT(0);
+	}
+
+	for(uint64_t i=0; i<5; i++)
+	{
+		if(relation->tuples[i].key != i || relation->tuples[i].rowId != 4-i)
+		{
+			CU_ASSERT(0);
+		}
+	}
+}
+
+
+/**
+ * Test case 5: Normal file with 1000 lines
+ */
+void testRelation_from_file5()
+{
+	char *filename = "/test_files/big_relation_test_file.txt";
+	relation *rel=relation_from_file(filename);
+	if(relation == NULL || relation->num_tuples != 1000 || relation->tuples == NULL)
+	{
+	    CU_ASSERT(0);
+	}
+
+	for(uint64_t i=0; i<1000; i++)
+	{
+		if(relation->tuples[i].key != i || relation->tuples[i].rowId != 999-i)
+		{
+			CU_ASSERT(0);
+		}
+	}
+}
+
+
 
 void testPrint_relation()
 {
@@ -90,6 +242,7 @@ int main()
     if((NULL==CU_add_test(pSuite, "testRead_from_file", testRead_from_file))||
             (NULL==CU_add_test(pSuite, "testDelete_table", testDelete_table))||
             (NULL==CU_add_test(pSuite, "testCreate_relation_from_table", testCreate_relation_from_table))||
+            (NULL==CU_add_test(pSuite, "testRelation_from_file", testRelation_from_file))||
             (NULL==CU_add_test(pSuite, "testPrint_relation", testPrint_relation))||
             (NULL==CU_add_test(pSuite, "testPrint_tuples", testPrint_tuples)))
     {

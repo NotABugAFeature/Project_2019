f56092f6b5278b7af88aec7e722ee1267976af36
George Koursiounis <36711755+gkoursiounis@users.noreply.github.com>
2019-11-04 13:01:20 +0000

    formatting updated

--- result_list.h
+++ result_list.h
@@ -22,16 +22,21 @@ typedef struct result_list_bucket result_list_bucket;
 typedef struct result_list_node result_list_node;
 typedef struct result_list result_list;
 
+
 /**
  * Creates an empty result list and returns a pointer to that list
  * @return result_list* The new list
  */
 result_list* create_result_list();
+
+
 /**
  * Deletes all the nodes of the list given
  * @param result_list* the list to delete
  */
 void delete_result_list(result_list*);
+
+
 /**
  * Adds a pair of row ids in the last bucket of the list and creates a new
  * bucket if needed.
@@ -41,27 +46,36 @@ void delete_result_list(result_list*);
  * @return int 0 If Successful
  */
 int append_to_list(result_list*, uint64_t r_row_id, uint64_t s_row_id);
+
+
 /**
  * Prints All The Nodes And Buckets Of The List From First To Last.
  * @param result_list The result list to print
  */
 void print_result_list(result_list*);
+
+
 /**
  * Returns if the list is empty.
  * @param result_list The res
  * @return int 1 if empty else 0
  */
 int is_result_list_empty(result_list* list);
+
+
 /**
  * Returns the number of nodes in the result list.
  * @param
  * @return int The number of nodes
  */
 unsigned int result_list_get_number_of_buckets(result_list*);
+
+
 /**
  * Returns the number of records in the result list.
  * @param
  * @return uint64_t The number of records
  */
 uint64_t result_list_get_number_of_records(result_list*);
+
 #endif /*CLIENTLIST_H*/

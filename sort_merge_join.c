db3c993f074bf0bcc1342968fa0a3aab80ff1b07
MariaKrm <maria.karamina@gmail.com>
2019-12-12 21:13:26 +0200

    Changed comments

--- sort_merge_join.c
+++ sort_merge_join.c
@@ -5,9 +5,10 @@
 
 /**
  * Implements the final join of two relations
- * and places the result into a 'result_list'
+ * and places the result into two middle lists
  *
- * @param list - result list
+ * @param list_t - middle list for first relation
+ * @param list_s - middle list for second relation
  * @param t - first relation
  * @param s - second relation
  * @return an integer code {0,1,2}
@@ -16,13 +17,13 @@ int final_join(middle_list* list_t, middle_list* list_s, relation *t, relation *
 {
     if(list_t==NULL||list_s==NULL||t==NULL||s==NULL||(t->num_tuples>0&&t->tuples==NULL)||(s->num_tuples>0&&s->tuples==NULL))
     {
-	fprintf(stderr, "%s", "final_join Error: arguments cannot be null\n");
+	    fprintf(stderr, "%s", "final_join Error: arguments cannot be null\n");
         return 1;
     }
     uint64_t z=0;
     for(uint64_t i = 0; i < t->num_tuples; i++)
     {
-	      uint64_t j = z;
+	    uint64_t j = z;
         while(j < s->num_tuples && t->tuples[i].key >= s->tuples[j].key)
         {
             if(t->tuples[i].key == s->tuples[j].key)
@@ -33,16 +34,16 @@ int final_join(middle_list* list_t, middle_list* list_s, relation *t, relation *
                     return 2;
                 }
 
-		            if(append_to_middle_list(list_s, s->tuples[j].row_id))
+		        if(append_to_middle_list(list_s, s->tuples[j].row_id))
                 {
                     fprintf(stderr, "%s", "final_join Error: append to list\n");
                     return 2;
                 }
             }
-	          else
-	          {
-		            z++;
-	          }
+	        else
+	        {
+                z++;
+	        }
             j++;
         }
     }
